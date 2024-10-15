7a8bac508eef5781e76a8cf33c960f11ad308503
Ramdhan Kumar <ramdhankumar1425@gmail.com>
2024-10-15 16:30:48 +0530

    none

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ const corsOptions = {
   method: "GET, POST, PUT, PATCH, DELETE",
   credentials: true,
 };
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
